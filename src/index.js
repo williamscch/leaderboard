369cb444a5b75558ce3a7c3894bcbaeaa5cfd5ee
wdavidcch <david.13.2121@gmail.com>
2022-07-21 11:09:20 -0400

    Add functions to work with the API and styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,57 @@
 import './index.css';
+
+const path = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/EWfiKQuijyVR2B4R0w9s/scores/';
+const list = document.getElementById('list');
+
+const renderLeaderboar = (data) => {
+  data.result.forEach((e) => {
+    const line = document.createElement('li');
+    const userName = document.createElement('h4');
+    const points = document.createElement('span');
+
+    list.appendChild(line);
+    line.append(userName, points);
+
+    userName.textContent = e.user;
+    points.textContent = e.score;
+  });
+};
+
+const getGameScores = async () => {
+  const response = await fetch(path);
+  console.log(response);
+  const data = await response.json();
+  // data.result.splice(0, 1);
+  data.result.sort((a, b) => b.score - a.score);
+  console.log(data);
+  renderLeaderboar(data);
+};
+
+const addNewScore = async (userName, points) => {
+  const response = await fetch(path,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user: userName,
+        score: points,
+      }),
+    }).then((data) => data.json());
+
+  const data = await response.json();
+};
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const userName = document.querySelector('.user-name').value;
+  const points = document.querySelector('.points').value;
+  addNewScore(userName, points);
+  // userName.value = null;
+  // points.value = null;
+});
+
+getGameScores();
---
+++ src/modules/create-game.js
@@ -0,0 +1,18 @@
+const newGamePost = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    name: 'Mortal Kombat',
+  }),
+};
+
+const createNewGame = async () => {
+  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', newGamePost).then((response) => response.json());
+};
+
+// Another way to it with the terminal
+// curl -X POST -d 'name=residentevil' https://us-central1-js-capstone-backend.cloudfunctions.net/api/games
+
+createNewGame();
